e077dd331c177c3a5f97db66aeb6f453e43e7c6e
Kalpana Karra <kkarra@stanford.edu>
2016-07-25 11:43:12 -0700

    Search in Progress..

--- client/jsx/components/yeast_labs/yeast_labs.jsx
+++ client/jsx/components/yeast_labs/yeast_labs.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
-import { connect } from 'react-redux';
+
+import SearchBar from '../widgets/search_bar.jsx';
+
+const COLLEAGUE_BASE_URL = '/backend/colleagues';
 
 const YeastLabsContainer = React.createClass({
-  render() {
-    return <h1>Yeast Labs Container</h1>
+
+  getInitialState () {
+    return {
+      searchResults: []
+    };
+  },
+
+  _renderSearchResults () {
+    return this.state.searchResults.map( (d,i) => {
+      return <p key={`labRes${i}`}>{d.last_name}</p>;
+    });
+  },
+
+  _renderSearchBar () {
+    var _onSubmit = query => {
+      this._fetchData(query);
+    }
+    return <SearchBar onSubmit={_onSubmit} />;
+  },
+
+  _fetchData (query) {
+    var url = `${COLLEAGUE_BASE_URL}?last_name=${query}`;
+    fetch(url).then( response => {
+      return response.json();
+    }).then( results => {
+      this.setState({ searchResults: results });
+    });
+  },
+
+  render () {
+    return (
+      <div>
+        <h1>Find a Yeast Lab</h1>
+        <hr />
+        {this._renderSearchBar()}
+        {this._renderSearchResults()}
+      </div>
+    );
   }
 });
 
-function mapStateToProps(_state) {
-  return {
-  };
-}
 
-module.exports = connect(mapStateToProps)(YeastLabsContainer);
+module.exports = YeastLabsContainer;
+
